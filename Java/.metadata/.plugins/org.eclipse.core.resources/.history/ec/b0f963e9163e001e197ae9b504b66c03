cc6e5f0ffdd6f14ae9aad3dc3643a21773f5d54f
GABRIELA SCHIRMER <gabriela_schirmer@estudante.sesisenai.org.br>
2023-08-18 21:40:37 -0300

    18/08

---
+++ Java/.metadata/.plugins/org.eclipse.core.resources/.history/ec/b0f963e9163e001e197ae9b504b66c03
@@ -0,0 +1,12 @@
+package br.com.unisenai.poo;
+
+public class Produto { //Propriedades de um produto de e-comerce
+	String nome;
+	double preco;
+	String tamanho;
+	String entrega;
+	String descricao;
+	double frete;
+	String avaliacao;
+	String cor;
+}
--- Java/POO/src/br/com/unisenai/poo/Produto.java
+++ Java/POO/src/br/com/unisenai/poo/Produto.java
@@ -1,12 +1,31 @@
 package br.com.unisenai.poo;
 
 public class Produto { //Propriedades de um produto de e-comerce
-	String nome;
-	double preco;
-	String tamanho;
-	String entrega;
-	String descricao;
-	double frete;
-	String avaliacao;
-	String cor;
+	public int idProduto;
+	public String nome;
+	public double preco;
+	public String tamanho;
+	public String entrega;
+	public String descricao;
+	public double frete;
+	public String avaliacao;
+	public String cor;
+}
+
+class Cliente {
+	public int idCliente;
+	public String nomeCliente;
+	public int numeroCartao;
+	public String endereco;
+	public String email;
+	public String telefone;
+}
+
+class Pedido{
+	public int numeroPedido;
+	public Produto produto;
+	public Cliente cliente;
+	public String previsaoEntrega;
+	public String status;
+	public double custoFinal;
 }
